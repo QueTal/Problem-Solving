ca1fcaecf67e24c80cff6828fab06a8692e6c6a1
quetal <gibum1995@gmail.com>
2021-05-11 19:14:42 +0900

    Daily Challenge 5/6

--- leetcode/#109-medium.cpp
+++ leetcode/#109-medium.cpp
@@ -10,7 +10,7 @@
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}œ
  * };
  */
 /**
