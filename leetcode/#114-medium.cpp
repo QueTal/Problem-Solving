1ed12aa2f698ca22d5995a7991e0882ca8190397
quetal <gibum1995@gmail.com>
2021-05-14 16:57:46 +0900

    Daily Challenge 5/14

---
+++ leetcode/#114-medium.cpp
@@ -0,0 +1,27 @@
+/* 
+ * 114. Flatten Binary Tree to Linked List
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev = nullptr;
+    void flatten(TreeNode* root) {
+        if(root == nullptr) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->left = nullptr; root->right = prev;
+        prev = root;
+    }
+};
