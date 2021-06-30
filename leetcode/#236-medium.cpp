b1ec0d72cbe42827df3e52d863ba6a548ee43095
quetal <gibum1995@gmail.com>
2021-06-30 17:13:46 +0900

    Daily Challenge 6/30

---
+++ leetcode/#236-medium.cpp
@@ -0,0 +1,27 @@
+/*
+ * 236. Lowest Common Ancestor of a Binary Tree
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return nullptr;
+        else if (root->val == p->val or root->val == q->val) return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+        if (left != nullptr and right != nullptr) return root;
+        return right == nullptr ? left : right;
+    }
+};
