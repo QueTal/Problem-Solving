4e476230de601e064e2c0ad84b45afa0c05cf9fb
keyBomb <gibum1995@gmail.com>
2021-10-20 23:39:46 +0900

    dijkstra basic

--- BOJ/#1753.cpp
+++ BOJ/#1753.cpp
@@ -1,24 +1,51 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+
+#define INF 987654321
 
 using namespace std;
-int arr[20001][20001];
+
+int dist[20001] = {0};
 
 int main(void){
-    cin.tie(0);
+    cin.tie(0); cout.tie(0);
     ios_base::sync_with_stdio(false);
-    int v, e, start;
-    cin >> v >> e >> start;
 
-    for (int i = 0; i < v; i++){
-        for (int j = 0; j < v; j++){
-            arr[i][j] = (i == j) ? 0 : INT_MAX;
+    int v, e, start;
+    cin >> v >> e;
+    cin >> start;
+    
+    vector< vector < pair<int, int> > > vv(v+1);
+
+    for (int i = 0; i < e; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        vv[a].push_back(make_pair(b, c));
+    }
+    for (int i = 1; i <= v; i++) dist[i] = INF;
+    dist[start] = 0;
+
+    priority_queue<pair<int, int> > pq;
+    pq.push(make_pair(0, start));
+    while (!pq.empty()){
+        int node = pq.top().second;
+        int val = -pq.top().first;
+        pq.pop();
+
+        for (auto n : vv[node]){
+            int next = n.first;
+            int nextVal = val + n.second;
+            if (nextVal < dist[next]){
+                dist[next] = nextVal;
+                pq.push(make_pair(-nextVal, next));
+            }
         }
     }
-    for (int i = 0; i < e; i++){
-        int f, t, w;
-        cin >> f >> t >> w;
-        arr[f-1][t-1] = min(w, arr[f-1][t-1]);
-    }
-
-    dijkstra();
+    
+    for (int i = 1; i <= v; i++) {
+		if (dist[i] == INF) cout << "INF\n";
+		else cout << dist[i] << "\n";
+	}
+    return 0;
 }
