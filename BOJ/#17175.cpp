ca8720afde8f3012b030c9a6d25c413808bac106
keyBomb <gibum1995@gmail.com>
2022-01-04 19:11:16 +0900

    Fibonacci renew

---
+++ BOJ/#17175.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int n;
+int dp[51];
+
+int main(){	
+	cin >> n;
+	dp[0] = 1; dp[1] = 1;
+	
+	for(int i=2; i<=n; i++){
+		dp[i] = (1 + dp[i-2] + dp[i-1])%1000000007;
+	}
+	
+	cout << dp[n];
+	
+	return 0;
+}
