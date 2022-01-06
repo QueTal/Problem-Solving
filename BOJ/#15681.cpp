4a8ebc410b171694405674bf448d428177bf4fca
keyBomb <gibum1995@gmail.com>
2022-01-06 13:00:16 +0900

    Memory: 12580 KB, Time: 72 ms

---
+++ BOJ/#15681.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, R, Q;
+vector<int> e[100001];
+bool visited[100001];
+int nums[100001];
+
+int dfs(int r){
+    if (nums[r] != 0) return nums[r];
+
+    visited[r] = true;
+
+    int cnt = 1;
+    for (int i = 0; i < e[r].size(); i++){
+        int next = e[r][i];
+        if (visited[next]) continue;
+        cnt += dfs(next);
+    }
+    nums[r] = cnt;
+    return cnt;
+}
+
+int main(void){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    cin >> N >> R >> Q;
+    
+    int u, v;
+    for (int i = 0; i < N-1; i++){
+        cin >> u >> v;
+        e[u].push_back(v);
+        e[v].push_back(u);
+    }
+
+    nums[R] = dfs(R);
+
+    int q;
+    for (int i = 0; i < Q; i++){
+        cin >> q;
+        cout << nums[q] << "\n";
+    }
+    return 0;
+}
