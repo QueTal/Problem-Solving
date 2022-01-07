4a38ae5c4a08069ae1f4ec642c0582830843132f
keyBomb <gibum1995@gmail.com>
2022-01-07 10:21:04 +0900

    Time: 16 ms (97.06 %), Space: 16.6 MB (80.97 %)

---
+++ leetcode/Algorithm/#382-medium.cpp
@@ -0,0 +1,38 @@
+/**
+ * 382. Linked List Random Node
+ * https://leetcode.com/problems/linked-list-random-node/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> v; 
+public:
+    Solution(ListNode* head) {
+        srand(time(nullptr));
+        while (head){
+            v.push_back(head->val);
+            head = head->next;
+        }
+    }
+    
+    int getRandom() {
+        int idx = rand()%v.size();
+        return v[idx];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
