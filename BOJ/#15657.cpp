2b6d61e1778debcf5920ad4aea3b483a981c9192
keyBomb <gibum1995@gmail.com>
2022-01-27 12:41:19 +0900

    Memory: 2584 KB, Time: 4 ms

---
+++ BOJ/#15657.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+int arr[10];
+bool visited[10];
+vector< vector<int> > answer;
+
+void dfs(int idx, vector<int>& v){
+    if (M == v.size()){
+        answer.push_back(v);
+        return;
+    }
+    for (int i = idx; i < N; i++){
+        v.push_back(arr[i]);
+        dfs(i, v); 
+        v.pop_back();
+    }
+}
+
+int main(void){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    
+    sort(arr, arr+N);
+    vector<int> v;
+    dfs(0, v);
+
+    sort(answer.begin(), answer.end());
+
+    for (int i = 0; i < answer.size(); i++){
+        for (int j = 0; j < M; j++){
+            cout << answer[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
