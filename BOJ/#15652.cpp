67eb7b759ef079721371b1e61dab7814e57a1c73
keyBomb <gibum1995@gmail.com>
2022-01-27 12:33:32 +0900

    Memory: 2020 KB, Time: 272 ms

---
+++ BOJ/#15652.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+bool visited[9];
+
+void dfs(int idx, vector<int>& v){
+    if (v.size() == M) {
+        for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+        cout << endl;
+        return;
+    }
+    
+    for (int i = idx; i <= N; i++){
+        v.push_back(i);
+        dfs(i, v);
+        v.pop_back();
+    }
+}
+int main(void){
+    cin >> N >> M;
+    vector<int> v;
+    dfs(1, v);
+    return 0;
+}
