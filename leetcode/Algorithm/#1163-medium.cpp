38a4fb200b59369a2cf5396c23bd734be99fc506
keyBomb <gibum1995@gmail.com>
2022-03-22 23:52:43 +0900

    Time: 37 ms (89.06 %), Space: 21 MB (99.12 %)

---
+++ leetcode/Algorithm/#1163-medium.cpp
@@ -0,0 +1,17 @@
+/**
+ * 1663. Smallest String With A Given Numeric Value
+ * https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/
+ */
+
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        string res(n,'a');
+        for(int i=0;i<n;++i){
+            int remain=k-(n-1-i);
+            if(remain>=26)  res[n-1-i]='z' , k-=26;
+            else res[n-1-i]=char('a'+remain-1) , k=n-i-1;
+        }
+        return res;
+    }
+};
